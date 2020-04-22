aa3cfb23ec76db5b61315d5cea31d1873fbfae6d
isabel k. lee <hello@isabelklee.co>
2020-04-22 13:07:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+})
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages() {
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(results => {
+        results.message.forEach(image => renderImages(image))
+    })
+    
+    // see the content inside the json object
+    // .then(json => console.log(json))
+}
+
+function renderImages(url) {
+    let div = document.querySelector('div#dog-image-container')
+    let list = document.createElement('ol')
+    let listItem = document.createElement('li')
+    let image = document.createElement('img')
+
+    image.src = url
+
+    div.appendChild(list)
+    list.appendChild(listItem)
+    listItem.appendChild(image)
+}
