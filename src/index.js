4622de476aafb0526bf67e01696c89b339d672db
isabel k. lee <hello@isabelklee.co>
2020-04-24 14:48:06 -0400

    add fetch dog breeds method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+let breeds = []
+
 document.addEventListener('DOMContentLoaded', function() {
     fetchImages()
+    fetchBreeds()
 })
+
+// challenge 1: render random dog images
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
@@ -31,4 +36,24 @@ function renderImages(url) {
 //     let listItem = document.createElement('li')
 //     listItem.innerHTML = `<img src="${url}">`
 //     orderedList.appendChild(listItem)
+// }
+
+// challenge 2: load all the dog breeds in an unordered list
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchBreeds() {
+    fetch(breedUrl)
+        .then(response => response.json())
+        .then(results => {
+            breeds = Object.keys(results.message)
+    })
+}
+
+// function renderBreeds(url) {
+//     let unorderedList = document.createElement('ul')
+//     let breed = document.createElement('li')
+//     breed.value = url
+
+//     unorderedList.appendChild(breed)
 // }
