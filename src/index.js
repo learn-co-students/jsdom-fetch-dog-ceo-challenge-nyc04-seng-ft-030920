3c5e571e34c9b4eb896bda41e5a239dfd2aae705
isabel k. lee <hello@isabelklee.co>
2020-04-25 23:06:07 -0400

    refactor random dog image method

--- src/index.js
+++ src/index.js
@@ -1,42 +1,21 @@
-let breeds = []
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetchImages()
-    fetchBreeds()
-})
-
 // challenge 1: render random dog images
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const dogImagesUrl = "https://dog.ceo/api/breeds/image/random/4"
+const dogContainer = document.querySelector('div#dog-image-container')
 
-function fetchImages() {
-    fetch(imgUrl)
-    .then(response => response.json())
-    .then(results => {
-        results.message.forEach(image => renderImages(image))
+fetch(dogImagesUrl)
+    .then(r => r.json())
+    .then((dogs) => {
+        dogs.message.forEach((dog) => {
+            loadDogs(dog)
     })
+})
+
+function loadDogs(dog) {
+    let dogImage = document.createElement('img')
+    dogImage.src = dog
+    dogContainer.appendChild(dogImage)
 }
-
-function renderImages(url) {
-    let dogContainer = document.querySelector('div#dog-image-container')
-    let image = document.createElement('img')
-
-    image.src = url
-
-    dogContainer.appendChild(image)
-}
-
-// garbage code
-
-// let dogs = document.querySelector('div#dog-image-container')
-// let orderedList = document.createElement('ol')
-// dog.appendChild(orderedList)
-
-// function renderImages(url) {
-//     let listItem = document.createElement('li')
-//     listItem.innerHTML = `<img src="${url}">`
-//     orderedList.appendChild(listItem)
-// }
 
 // challenge 2: load all the dog breeds in an unordered list
 
