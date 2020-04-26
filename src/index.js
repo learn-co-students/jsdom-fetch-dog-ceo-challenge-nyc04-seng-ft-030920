f132891ba95f481b8be95265d1eabfebec467082
isabel k. lee <hello@isabelklee.co>
2020-04-25 23:30:45 -0400

    create method for loading all dog breeds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // challenge 1: render random dog images
 
 const dogImagesUrl = "https://dog.ceo/api/breeds/image/random/4"
+const dogBreedsUrl = 'https://dog.ceo/api/breeds/list/all'
 const dogContainer = document.querySelector('div#dog-image-container')
 
 fetch(dogImagesUrl)
@@ -8,8 +9,8 @@ fetch(dogImagesUrl)
     .then((dogs) => {
         dogs.message.forEach((dog) => {
             loadDogs(dog)
+        })
     })
-})
 
 function loadDogs(dog) {
     let dogImage = document.createElement('img')
@@ -19,20 +20,20 @@ function loadDogs(dog) {
 
 // challenge 2: load all the dog breeds in an unordered list
 
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+fetch(dogBreedsUrl)
+    .then(r => r.json())
+    .then((breeds) => {
+        breeds = Object.keys(breeds.message)
+        loadBreeds(breeds)
+    })
 
-function fetchBreeds() {
-    fetch(breedUrl)
-        .then(response => response.json())
-        .then(results => {
-            breeds = Object.keys(results.message)
+function loadBreeds(breeds) {
+    let unorderedList = document.createElement('ul')
+    dogContainer.appendChild(unorderedList)
+
+    breeds.forEach((breedName) => {
+        let listItem = document.createElement('li')
+        listItem.innerText = breedName
+        unorderedList.appendChild(listItem)
     })
 }
-
-// function renderBreeds(url) {
-//     let unorderedList = document.createElement('ul')
-//     let breed = document.createElement('li')
-//     breed.value = url
-
-//     unorderedList.appendChild(breed)
-// }
