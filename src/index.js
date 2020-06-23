9f3ad156b6d16bffc34b7f28de17f119cdbf10aa
isabel k. lee <hello@isabelklee.co>
2020-06-23 13:26:59 -0400

    loaded all dog breeds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 const dogImageContainer = document.getElementById("dog-image-container")
+const breedsList = document.getElementById("dog-breeds")
 
 fetch(imgUrl)
 .then(r => r.json())
@@ -17,4 +19,22 @@ function renderDogImages(dog) {
   let image = document.createElement("img")
   image.src = dog
   dogImageContainer.append(image)
+}
+
+fetch(breedUrl)
+.then(r => r.json())
+.then((breeds) => {
+  forEachBreed(breeds)
+})
+
+function forEachBreed(breeds) {
+  Object.keys(breeds.message).forEach((breed) => {
+    renderBreeds(breed)
+  })
+}
+
+function renderBreeds(breed) {
+  let listItem = document.createElement("li")
+  listItem.innerText = breed
+  breedsList.append(listItem)
 }
